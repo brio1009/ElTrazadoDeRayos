45b424b37d97539aad3db7aedabf4761b8cb2342
Philipp Bausch <bauschp@informatik.uni-freiburg.de>
2014-05-02 18:50:13 +0200

    Fixed a Header Problem in Ellipsoid.cpp

--- src/RayTracerLib/Ellipsoid.cpp
+++ src/RayTracerLib/Ellipsoid.cpp
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-#include "./Sphere.h"
+#include "./Ellipsoid.h"
 #include "./Ray.h"
 #include <glm/glm.hpp>
 
