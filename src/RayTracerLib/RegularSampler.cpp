8a165708deaefef9f9e990e332e267c0e595f5da
Christoph Gissler <christoph1009@gmail.com>
2014-06-04 15:10:46 +0200

    first work on propertymanager

--- src/RayTracerLib/RegularSampler.cpp
+++ src/RayTracerLib/RegularSampler.cpp
@@ -22,8 +22,10 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-#include <glm/glm.hpp>
+
 #include "./RegularSampler.h"
+
+#include <glm/glm.hpp>
 #include "./Sampler.h"
 #include "./Ray.h"
 
