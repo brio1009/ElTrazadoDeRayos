d6ef00f42c01c22ed8d5b20cb9f9cac257c77069
Philipp Bausch <bauschp@informatik.uni-freiburg.de>
2014-08-16 14:55:46 +0200

    Added genfactory.

---
+++ include/genfactory/GenericFactory.h
@@ -0,0 +1,330 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2014 CantTouchDis <bauschp@informatik.uni-freiburg.de>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+#ifndef GENERICFACTORY_H_
+#define GENERICFACTORY_H_
+
+#include <typeinfo>
+#include <cstdio>
+#include <string>
+#include <type_traits>
+#include <map>
+
+#ifndef DISABLELITERALSTRING
+#include "./LiteralStringList.h"
+#endif  // DISABLELITERALSTRING
+
+#include "./Property.h"
+
+namespace genfactory {
+// These are helper structs to be able to distinquish between existing and non
+// existing members. Check out:
+// stackoverflow.com/questions/13786888/check-if-member-exists-using-enable-if
+struct BasicCase { };
+struct SpecialCase : BasicCase { };
+template<typename> struct OkCase { typedef int type; };
+
+/// There two are helper functions to check wheter or not the base has a create
+/// method. This is important, cause all stored pointers are of this type and
+/// create has to exist in order to create objects of subclasses.
+template<typename Base, typename OkCase<decltype(&Base::create)>::type = 0>
+Base* creationHelper(
+      const std::string& name,
+      std::map<std::string, Base*> reflectionMap,
+      SpecialCase) {
+  Base* creator = reflectionMap[name];
+  if (!creator)
+    return nullptr;
+  return creator->create();
+}
+/// See above. this is the case if Base doesnt have create().
+template<typename Base>
+Base* creationHelper(
+      const std::string&,
+      std::map<std::string, Base*>,
+      BasicCase) {
+  return nullptr;
+}
+
+/// This helper class is used to delete the pointers of the static
+/// map when the programm terminates. (Composite)
+template<typename Key, typename Value>
+class HelperPointerMap {
+ public:
+  ~HelperPointerMap() {
+    auto it = m_Map.begin();
+    auto endIt = m_Map.end();
+    for (; it != endIt; ++it) {
+      delete it->second;
+    }
+  }
+  std::map<Key, Value*>& map() {
+    return m_Map;
+  }
+ private:
+  std::map<Key, Value*> m_Map;
+};
+
+/// This class provides a simple interface to create and modify classes that
+/// provide the nessesary components to be modified. Below this definition
+/// there will be MACROS to help creating such classes.
+template<typename Base>
+class GenericFactory {
+ public:
+  /// Static field to import everything at compiletime.
+  static const char helpInit;
+
+  /// Registeres all classes for base (has to be user defined).
+  static char registerAllForBase();
+  /// Use this method to create a object of class name.
+  static Base* create(const std::string& name);
+
+  /// Calls the method with given name on the object to set a property value.
+  static void setProperty(
+        const std::string& propName,
+        Base* const objPtr,
+        const std::string& value);
+
+  /// Calls the method with given name on the object to get a property value.
+  static std::string getProperty(
+        const std::string& propName,
+        Base* const objPtr);
+
+  /// Registers the class in GenericFactory<Base> of type C
+  /// There will be errors if C is not of type Base,
+  /// If C is not DefaultConstructable,
+  /// And if C has a pure member. (abstract).
+  template<typename C>
+  static typename std::enable_if<
+      !std::is_abstract<C>::value
+      && std::is_default_constructible<C>::value, void>::type
+        registerClass();
+  /// Registers just the properties but doesnt add the class to the
+  /// constructables.
+  template<typename C>
+  static typename std::enable_if<
+      std::is_abstract<C>::value
+      || !std::is_default_constructible<C>::value, void>::type
+    registerClass();
+  /// Registers a method of C to be called by the name methodName.
+  /// Use callMethod(...) to call the method on a object.
+  template<typename C, typename Type>
+  static void registerProperty(
+        const std::string& methodName,
+        void (C::*setPtr)(Type),
+        Type (C::*getPtr)() const);
+
+ private:
+  /// We dont want anyone to create this.
+  GenericFactory();
+  /// This is the map that holds all the registered classes.
+  static std::map<std::string, Base*>& reflectionMap();
+
+  /// This is the map that holds all registered properties.
+  static std::map<std::string, Property<Base>*>& properyMap();
+
+  template<
+        typename C,
+        typename OkCase<decltype(&C::registerProperties)>::type = 0>
+  static void helpRegisterProperties(SpecialCase s);
+
+  template<typename C>
+  static void helpRegisterProperties(BasicCase b);
+  template<
+        typename C,
+        typename OkCase<decltype(C::name)>::type = 0,
+        typename OkCase<decltype(&C::create)>::type = 0>
+  static void helpRegisterClass(SpecialCase s);
+  // Dont register if u cant find the name.
+  template<typename C>
+  static void helpRegisterClass(BasicCase b);
+
+  template<typename C>
+  static void registerClassWithName(const char* const name);
+  template<typename C>
+  static void registerClassWithName(const std::string& name);
+#ifndef DISABLELITERALSTRING
+  template<typename C>
+  static void registerClassWithName(const literal_str_list& name);
+#endif  // DISABLELITERALSTRING
+};
+
+// #########################DEFINITIONS#########################################
+// Definitions to auto initialize the reflection maps.
+template<typename Base>
+const char GenericFactory<Base>::helpInit =
+      GenericFactory<Base>::registerAllForBase();
+// Used to produce compiler errors when not defined for used base.
+template<typename Base>
+char GenericFactory<Base>::registerAllForBase() {
+  static_assert(sizeof(Base) != sizeof(Base), "Specialize a"
+      "this method to register all of your used subtypes ob Base!");
+  return 'n';
+}
+
+// Definitions to access the static maps.
+template<typename Base>
+std::map<std::string, Base*>& GenericFactory<Base>::reflectionMap() {
+  static HelperPointerMap<std::string, Base> m_ReflMap;
+  return m_ReflMap.map();
+}
+
+template<typename Base>
+std::map<std::string, Property<Base>*>& GenericFactory<Base>::properyMap() {
+  static HelperPointerMap<std::string, Property<Base> > m_PropMap;
+  return m_PropMap.map();
+}
+// Definitions to register the classes and properties.
+template<typename Base>
+template<
+      typename C,
+      typename OkCase<decltype(&C::registerProperties)>::type>
+void GenericFactory<Base>::helpRegisterProperties(SpecialCase) {
+  // call register Properties.
+  C::registerProperties();
+}
+
+template<typename Base>
+template<typename C>
+void GenericFactory<Base>::helpRegisterProperties(BasicCase) {
+  fprintf(stderr, "Couldn't find static void %s::registerProperties()\n",
+      convert_to_string(C::name).c_str());
+}
+template<typename Base>
+template<typename C, typename Type>
+void GenericFactory<Base>::registerProperty(
+      const std::string& methodName,
+      void (C::*setPtr)(Type),
+      Type (C::*getPtr)() const) {
+  if (properyMap().find(methodName) != properyMap().end()) {
+    perror("There already exists a property with this name\n");
+    delete properyMap()[methodName];
+  }
+  properyMap()[methodName] = new TypeProperty<Base, C, Type>(setPtr, getPtr);
+}
+
+template<typename Base>
+template<typename C>
+typename std::enable_if<
+      std::is_abstract<C>::value
+      || !std::is_default_constructible<C>::value, void>::type
+        GenericFactory<Base>::registerClass() {
+  // TODO(bauschp, Fr 15. Aug 14:16:50 CEST 2014): register all properties.
+  GenericFactory<Base>::helpRegisterProperties<C>(SpecialCase());
+}
+template<typename Base>
+template<typename C>
+typename std::enable_if<
+      !std::is_abstract<C>::value
+      && std::is_default_constructible<C>::value, void>::type
+        GenericFactory<Base>::registerClass() {
+  // perform some checks so we dont run into a mess later on.
+  static_assert(std::is_base_of<Base, C>::value,
+        "C dosn`t have base Base\n");
+  helpRegisterClass<C>(SpecialCase());
+  // TODO(bauschp, Fr 15. Aug 14:16:50 CEST 2014): register all properties.
+  GenericFactory<Base>::helpRegisterProperties<C>(SpecialCase());
+}
+
+template<typename Base>
+template<
+      typename C,
+      typename OkCase<decltype(C::name)>::type,
+      typename OkCase<decltype(&C::create)>::type>
+void GenericFactory<Base>::helpRegisterClass(SpecialCase) {
+  registerClassWithName<C>(C::name);
+}
+
+template<typename Base>
+template<typename C>
+void GenericFactory<Base>::helpRegisterClass(BasicCase) {
+  fprintf(stderr, "(%s) is missing the static field with the reflection "
+      "name or Base* create() const member. Make sure u add them!!.\n",
+      typeid(C).name());
+}
+
+#ifndef DISABLELITERALSTRING
+template<typename Base>
+template<typename C>
+void GenericFactory<Base>::registerClassWithName(
+      const literal_str_list& name) {
+  registerClassWithName<C>(convert_to_string(name));
+}
+#endif  // DISABLELITERALSTRING
+
+template<typename Base>
+template<typename C>
+void GenericFactory<Base>::registerClassWithName(
+      const char* const name) {
+  registerClassWithName<C>(std::string(name));
+}
+
+template<typename Base>
+template<typename C>
+void GenericFactory<Base>::registerClassWithName(
+      const std::string& name) {
+  // This is used to register them automaticly.
+  if (sizeof(helpInit) != sizeof(helpInit) && helpInit)
+    return;
+  printf("REGISTERING %s.\n", name.c_str());
+  if (reflectionMap().find(name) != reflectionMap().end()) {
+    perror("There already exists a class with this name\n");
+    delete reflectionMap()[name];
+  }
+  // only default constructable C will land here.
+  // just construct one.
+  reflectionMap()[name] = new C();
+}
+
+// Definitions to access a property. (get/set).
+template<typename Base>
+void GenericFactory<Base>::setProperty(
+        const std::string& propName,
+        Base* const objPtr,
+        const std::string& value) {
+  Property<Base>* prop = properyMap()[propName];
+  if (prop) {
+    prop->set(objPtr, value);
+    return;
+  }
+  perror("There is no propery with this name.");
+}
+
+template<typename Base>
+std::string GenericFactory<Base>::getProperty(
+        const std::string& propName,
+        Base* const objPtr) {
+  Property<Base>* prop = properyMap()[propName];
+  if (prop)
+    return prop->get(objPtr);
+  perror("There is no propery with this name.");
+  return "ERROR";
+}
+
+// Definition to create a object with given name.
+template<typename Base>
+Base* GenericFactory<Base>::create(const std::string& name) {
+  // Thanks to registerClass only constructable objects will be called here.
+  return creationHelper(name, reflectionMap(), SpecialCase());
+}
+}  // namespace genfactory
+#endif  // GENERICFACTORY_H_
---
+++ include/genfactory/Property.h
@@ -0,0 +1,125 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2014 CantTouchDis <bauschp@informatik.uni-freiburg.de>
+// Copyright (c) 2014 brio1009 <christoph1009@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+#ifndef PROPERTY_H_
+#define PROPERTY_H_
+
+#include <cstdio>
+#include <string>
+
+namespace genfactory {
+/// A property consists of getter and setter.
+template<typename Base>
+class Property {
+ public:
+  virtual void set(Base* const obj, const std::string& value) const = 0;
+  virtual std::string get(const Base* const obj) const = 0;
+  virtual ~Property() { }
+};
+
+/// Helper struct to partially specialize.
+template<typename ValueType>
+struct StringCastHelper {
+ public:
+  /// Returns the value of the property as string.
+  static std::string toString(const ValueType& value);
+
+  /// Returns the value from string.
+  static ValueType fromString(const std::string& value);
+
+ private:
+  /// Hide the Constructor.
+  StringCastHelper();
+};
+
+/// Proeprty specialized for one ValueType.
+template<typename Base, typename OwnerClass, typename ValueType>
+class TypeProperty : public Property<Base> {
+ public:
+  /// Constructor setting the getter and setter ptr.
+  TypeProperty(void (OwnerClass::*pSetter)(ValueType value),
+               ValueType (OwnerClass::*pGetter)() const)
+    : setterPtr(pSetter),
+      getterPtr(pGetter) { }
+
+  virtual void set(Base* const obj, const std::string& value) const override {
+    OwnerClass* const me = dynamic_cast<OwnerClass* const>(obj);
+    if (me) {
+      set(me, value);
+      return;
+    }
+    perror("Cant call this prop on this Object!\n");
+  }
+  virtual std::string get(const Base* const obj) const override {
+    const OwnerClass* const me = dynamic_cast<const OwnerClass* const>(obj);
+    if (me)
+      return get(me);
+    perror("Cant call this prop on this Object!\n");
+    return "ERRORINPROP";
+  }
+
+  /// Returns the value of the property as string.
+  std::string get(const OwnerClass* const objPtr) const {
+    return StringCastHelper<ValueType>::toString(getValue(objPtr));
+  }
+
+  /// Sets the value from string.
+  void set(OwnerClass* const objPtr, const std::string& value) const {
+    setValue(objPtr, StringCastHelper<ValueType>::fromString(value));
+  }
+
+ private:
+  /// Getter for the property.
+  ValueType getValue(const OwnerClass* const objPtr) const {
+    return (objPtr->*getterPtr)();
+  }
+
+  /// Setter for the property.
+  void setValue(OwnerClass* const objPtr, ValueType value) const {
+    (objPtr->*setterPtr)(value);
+  }
+  /// Saves pointer to the setter (offset to class pointer).
+  void (OwnerClass::*setterPtr)(ValueType value);
+  /// Saves pointer to the getter (offset to class pointer).
+  ValueType (OwnerClass::*getterPtr)() const;
+};
+
+// #########################DEFINITIONS#########################################
+
+template<typename ValueType>
+std::string StringCastHelper<ValueType>::toString(const ValueType&) {
+  // this sizeof trick is a lottle bit dirty. But false will allways cause this
+  // exception.
+  static_assert(sizeof(ValueType) != sizeof(ValueType), "Specialize a"
+      "StringCastHelper for your ValueType!");
+}
+
+template<typename ValueType>
+ValueType StringCastHelper<ValueType>::fromString(const std::string&) {
+  // this sizeof trick is a lottle bit dirty. But false will allways cause this
+  // exception.
+  static_assert(sizeof(ValueType) != sizeof(ValueType), "Specialize a"
+      "StringCastHelper for your ValueType!");
+}
+}  // namespace genfactory
+#endif  // PROPERTY_H_
