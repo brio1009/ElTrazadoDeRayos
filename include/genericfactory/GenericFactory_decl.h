4ac08d4158b79d23a6fb8a14f09d6aeb2c6121f8
Philipp Bausch <bauschp@informatik.uni-freiburg.de>
2014-09-04 20:59:38 +0200

    Maybe now^2?

--- include/genericfactory/GenericFactory_decl.h
+++ include/genericfactory/GenericFactory_decl.h
@@ -127,7 +127,7 @@ class GenericFactory {
   template<
         typename C,
         typename OkCase<decltype(C::name)>::type = 0,
-        typename OkCase<decltype(C::create)*>::type = 0>
+        typename OkCase<decltype(&C::create)>::type = 0>
   static void helpRegisterClass(SpecialCase s);
   // Dont register if u cant find the name.
   template<typename C>
