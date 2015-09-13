7dd5d7601e1143a4487b2c77e97022b0efb2b5c1
brio1009 <christoph1009@gmail.com>
2015-09-13 17:13:19 +0200

    replaced premake 4 beta 5 with premake 5

--- projects/BuildLinuxProjects.sh
+++ projects/BuildLinuxProjects.sh
@@ -3,9 +3,9 @@
 UNAME=`uname`
 
 if [ "$UNAME" == "Linux" ]; then
-  chmod 700 ./premake4b5_linux
-  ./premake4b5_linux clean
-  ./premake4b5_linux gmake
+  chmod 700 ./premake5_linux
+  ./premake5_linux clean
+  ./premake5_linux gmake
 else
   echo "You are not using Linux.\n"
 fi
--- projects/BuildMacOSXProjects.sh
+++ projects/BuildMacOSXProjects.sh
@@ -3,11 +3,11 @@
 UNAME=`uname`
 
 if [ "$UNAME" == "Darwin" ]; then
-  chmod 700 ./premake4b5_macosx
-  ./premake4b5_macosx clean
-  ./premake4b5_macosx gmake
-  ./premake4b5_macosx xcode3
-  ./premake4b5_macosx xcode4
+  chmod 700 ./premake5_macosx
+  ./premake5_macosx clean
+  ./premake5_macosx gmake
+  ./premake5_macosx xcode3
+  ./premake5_macosx xcode4
 else
   echo "You are not using MacOSX.\n"
 fi
