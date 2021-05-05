bebcceb6188bf83f2461afc796cd64c9a013bf52
brio1009 <christoph1009@gmail.com>
2021-05-05 15:16:17 +0200

    removing now unused stuff

--- projects/BuildWindowsProjects.bat
+++
@@ -1,9 +0,0 @@
-# premake5_windows clean
-# premake5_windows vs2005
-# premake5_windows vs2008
-# premake5_windows vs2010
-# premake5_windows vs2012
-premake5_windows vs2013
-# premake5_windows vs2015
-# premake5_windows codeblocks
-# premake5_windows codelite
